e2355a8c0618c199efe7bc5ecffea0ba2ff5efb1
Kim <alsrb001218@naver.com>
2020-05-02 23:41:10 +0900

    globalRouter / controller 연결

--- globalController.js
+++ globalController.js
@@ -1,3 +1,18 @@
 export const home = (req, res) => {
   res.render("home", { pageTitle: "home" });
 };
+export const join = (req, res) => {
+  res.render("join", { pageTitle: "join" });
+};
+export const login = (req, res) => {
+  res.render("login", { pageTitle: "login" });
+};
+export const lectureDetail = (req, res) => {
+  res.render("lectureDetail", { pageTitle: "lectureDetail" });
+};
+export const userDetail = (req, res) => {
+  res.render("userDetail", { pageTitle: "userDetail" });
+};
+export const writeNotice = (req, res) => {
+  res.render("writeNotice", { pageTitle: "writeNotice" });
+};
--- globalRouter.js
+++ globalRouter.js
@@ -1,7 +1,19 @@
 import express from "express";
 import routes from "./routes";
-import { home } from "./globalController";
+import {
+  home,
+  join,
+  lectureDetail,
+  login,
+  userDetail,
+  writeNotice,
+} from "./globalController";
 
 export const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.lectureDetail(), lectureDetail);
+globalRouter.get(routes.userDetail, userDetail);
+globalRouter.get(routes.writeNotice, writeNotice);
